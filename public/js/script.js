87d1213cf19b1031a257cfed489261fd27503181
applewhi <applewhi@gmail.com>
2018-04-27 16:37:59 -0700

    Initial commit.

---
+++ public/js/script.js
@@ -0,0 +1,37 @@
+// Drum Arrays
+let kicks = [false,false,false,false,false,false,false,false,false,false,false,false,false,
+  false,false,false];
+let snares = [false,false,false,false,false,false,false,false,false,false,false,false,false,
+  false,false,false];
+let hiHats = [false,false,false,false,false,false,false,false,false,false,false,false,false,
+  false,false,false];
+let rideCymbals = [false,false,false,false,false,false,false,false,false,false,false,false,false,
+  false,false,false];
+
+function toggleDrum(drumArray,indexNum){
+
+  if (isValidDrum(drumArray) && isValidIndex(indexNum)){
+    if(drumArray.push[indexNum] === false){
+      drumArray.push[indexNum] = true;
+      }
+      drumArray.push[indexNum] = false;
+      }
+  return
+}
+
+function isValidDrum(drumArray){
+  return  drumArray === kicks || drumArray === snaresdrumArray ||
+  drumArray === hiHats || rideCymbals === rideCymbals;
+}
+
+function isValidIndex(indexNum){
+  return indexNum >= 1 && indexNum <= 16;
+}
+
+function clear(drumArray){
+  drumArray.forEach(
+      function(indexNum){
+        index = false;
+      }
+  )
+}
