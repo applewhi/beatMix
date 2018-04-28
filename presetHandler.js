cb9c1c54409b47d4ab3de6d3561d54771c25120e
applewhi <38227492+applewhi@users.noreply.github.com>
2018-04-28 02:03:47 -0700

    corrected the return array for the second element.

--- presetHandler.js
+++ presetHandler.js
@@ -1,24 +1,29 @@
 // Use this presets array inside your presetHandler
 const presets = require('./presets');
 
-//possibly should consolidate the notFound and found arrays of presetHandler() by creating one here.
 
 // Complete this function:
 const presetHandler = (requestMethodType,indexForPresetArray, newPresetArray) => {
-  if(indexForPresetArray < 0 || indexForPresetArray >= presets.length){
+ 
+  //possibly should consolidate the various array declarations/variables (e.g., notFound, found,
+  //and badRequest) of presetHandler() by creating one here.
+  
+  if(indexForPresetArray < 0 || indexForPresetArray >= presets.length || !Math.isInteger(indexForPresetArray)){
     return let notFound = [404];
   }
   else if {
             if (requestMethodType !== 'GET' && requestMethodType !== 'PUT'){
               let badRequest = [400];
+              return badRequest;
             }
           }
+  //else request method type is a valid one (GET or PUT)
   let found = [200];
   if(requestMethodType === 'GET'){
-    return found[indexForPresetArray].push(requestMethodType);
+    return found[1].push(presets[indexForPresetArray]);
   } //else requestMethodType = PUT
-  found[indexForPresetArray].push(newPresetArray);
-  return = found[1].push(newPresetArray);
+  presets[indexForPresetArray].push(newPresetArray);
+  return found[1].push(newPresetArray);
 };
 
 // Leave this line so that your presetHandler function can be used elsewhere:
